5f02cc7a04a50801ae8ba9c53bc5df26d470ab0f
Chigozie francis <81627242+francisChigozie@users.noreply.github.com>
2024-03-29 14:31:04 +0100

    second commit

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('public/public/build'))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/public' ,'build', 'index.html')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public' , 'public','build', 'index.html')))
 }
 
 app.all('*', (req, res, next) => {
