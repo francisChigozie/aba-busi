e9d851169972ae7d0800f80c0df89f68ef9c9ea3
Chigozie francis <81627242+francisChigozie@users.noreply.github.com>
2024-03-29 15:21:33 +0100

    second commit

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use('/api/review', reviewsRauter)
 // Serve Static Access in Production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('public/public'))
+  app.use(express.static('public'))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public','build', 'index.html')))
 }
